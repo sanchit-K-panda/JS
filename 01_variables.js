a0b769f090f0dc3825fabbbd097d335bf958b725
Sanchit <sleeppypanda0@gmail.com>
2026-01-29 16:51:27 +0530

    +

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 3232
+let accountEmail = "sanchitdono3@gmail.com"
+var accountPassword = 5656
+ /*
+var was used in old times - issue in block scope and functional scope
+*/
+accountId = "Jaipur"// not a good way to assign variable
+
+//accountId = 33
+
+//console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountId]);
---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newr version
+
+// alert (3-3) // we are using node js, not browser, will not work
+
+console.log("hi");
+
+// clean code - code readability should be givin priority (tc39.es / mdn - js documentation / standards)
+
+console.log("123");
+
+// Data types
+
+let name = "sanchit" // string
+
+let number = 34 // int
+
+let isUserLoggedIn = true // boolean
+
+let state;
+
+// null- standalone value
+// undefined- value not assigned
+// symbol - uniqueness
+
+// objects
+
+console.log(typeof(null));
+
