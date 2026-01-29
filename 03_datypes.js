213e58e582af4819233a917272713b806cb1ce8b
Sanchit <sleeppypanda0@gmail.com>
2026-01-29 19:57:20 +0530

    conversion of dp

---
+++ 03_datypes.js
@@ -0,0 +1,16 @@
+// conversion and operations
+
+let score = "33"
+
+console.log(typeof (score));
+
+let valueInScore = Number(score)
+
+console.log(typeof (valueInScore));
+ 
+// 33 is converted to 33
+// 33abc is converted to NaN
+// null is 0
+//undefinedd is NaN
+// 1,0 is true,false
+// "" is false "ff" is true
