76b5a66e7f5832a5b733bc19bb5f57d6e33d522d
Sanchit <sleeppypanda0@gmail.com>
2026-01-30 13:52:06 +0530

    strings

---
+++ 05_strings.js
@@ -0,0 +1,7 @@
+const name = "sanchit"
+const marks = "33/40"
+
+console.log(`hello my name is ${name} and my marks are ${marks}`);
+
+const newName = new String ('sanchi')
+console.log(name.toUpperCase());
