a0f8b68e755e07167f85a96be13495c9b7829f99
Sanchit <sleeppypanda0@gmail.com>
2026-01-30 12:36:19 +0530

    ..

---
+++ 04_memory.js
@@ -0,0 +1,31 @@
+// Stack (primitive datatypes) , Heap (non-primitive)
+
+// example
+
+// let userName = "sanchit"
+// let userTwoName = userName
+// userTwoName = "tom"
+
+// console.log(userName);
+
+// console.log(userTwoName);
+
+// Heap example
+
+let userInfo = {
+
+    userMail: "lol@gmail.com",
+    userNumber: 98989898
+
+};
+
+userTwoInfo = userInfo
+
+userTwoInfo.userMail = "lmao@gmail.com"
+
+console.log(userTwoInfo.userMail);
+
+
+
+
+
