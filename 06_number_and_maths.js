ed47599b0f7c157fa7c798a5b2a0118d7191f14c
Sanchit <sleeppypanda0@gmail.com>
2026-02-02 16:55:58 +0530

    ..

---
+++ 06_number_and_maths.js
@@ -0,0 +1,26 @@
+   const score = 400;
+// console.log(score);
+
+   const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2));
+
+   const otherNumber = 234.74343
+
+// console.log(otherNumber.toPrecision(3));
+
+  const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// // +++++++++++++++ Maths ++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+//round - give round off num
+//ceil - highest round off floor - lowest round off
+
+// Math.random = 0 to 1 values
+
+ 
